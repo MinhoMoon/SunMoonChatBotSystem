7006fa7975c2e101f0fa9add68defafab905e6ca
PC <idmmm134@naver.com>
2021-06-12 15:01:38 +0900

    getFood() Update

--- app/src/main/java/com/example/sunmoonchatbot/Data.java
+++ app/src/main/java/com/example/sunmoonchatbot/Data.java
@@ -3,7 +3,7 @@ package com.example.sunmoonchatbot;
 public class Data {
     public String getFood()
     {
-        String food = "오늘 추천메뉴다!";
+        String food = "오늘의 추천 음식점입니다!\n언니네식당에서 한끼는 어떠신가요?";
         return food;
     }
 }
